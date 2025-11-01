2b8fb44b06bbc0828e4b8bbb185c07a9a9a1e153
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-01 20:16:23 -0300

    build: infra + src py

--- infra/modules/dataflow/iam/main.tf
+++ infra/modules/dataflow/iam/main.tf
@@ -1,11 +1,11 @@
 resource "google_storage_bucket_iam_member" "dataflow_temp" {
-  bucket = replace("gs://cars-sales-${var.project_id}-${var.env}-dataflow-temp/", "gs://", "")
+  bucket = "cars-sales-${var.project_id}-${var.env}-dataflow-temp" 
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
 }
 
 resource "google_storage_bucket_iam_member" "dataflow_staging" {
-  bucket = replace("gs://cars-sales-${var.project_id}-${var.env}-events-staging/", "gs://", "")
+  bucket = "cars-sales-${var.project_id}-${var.env}-events-staging"  
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
 }
