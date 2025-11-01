a47bdfa3ef58f28843f599b10129354c2ed3cc80
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-01 20:09:32 -0300

    build: infra + src py

--- infra/modules/dataflow/iam/main.tf
+++ infra/modules/dataflow/iam/main.tf
@@ -1,22 +1,17 @@
-# Obtém o e-mail do service account autenticado
-data "google_client_config" "current" {}
-
-# IAM para buckets
 resource "google_storage_bucket_iam_member" "dataflow_temp" {
   bucket = replace("gs://cars-sales-${var.project_id}-${var.env}-dataflow-temp/", "gs://", "")
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${data.google_client_config.current.email}"
+  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
 }
 
 resource "google_storage_bucket_iam_member" "dataflow_staging" {
   bucket = replace("gs://cars-sales-${var.project_id}-${var.env}-events-staging/", "gs://", "")
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${data.google_client_config.current.email}"
+  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
 }
 
-# IAM para Pub/Sub
 resource "google_pubsub_topic_iam_member" "dataflow_pubsub" {
   topic  = "${var.project_id}-${var.env}-events"
   role   = "roles/pubsub.subscriber"
-  member = "serviceAccount:${data.google_client_config.current.email}"
+  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
 }
