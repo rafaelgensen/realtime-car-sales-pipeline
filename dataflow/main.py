7b6b2ab3d7cefac605b3be965977a64560e77851
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 16:01:59 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -3,8 +3,9 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, SetupOptions
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
+
 INPUT_SUB = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
-OUTPUT_PATH = f"gs://cars-sales-{PROJECT_ID}-prod-events-staging/events/output"
+OUTPUT_PREFIX = f"gs://cars-sales-{PROJECT_ID}-prod-events-staging/events/output"
 
 def run():
     options = PipelineOptions(
@@ -20,30 +21,18 @@ def run():
     from utils.transforms import process_event
 
     with beam.Pipeline(options=options) as p:
-        events = (
+        (
             p
             | "Read" >> beam.io.ReadFromPubSub(subscription=INPUT_SUB)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
             | "Parse" >> beam.Map(json.loads)
             | "Process" >> beam.Map(process_event)
             | "FilterNone" >> beam.Filter(lambda e: e is not None)
-        )
-
-        windowed = events | "Window" >> beam.WindowInto(
-            beam.window.FixedWindows(10),
-            trigger=beam.trigger.AfterProcessingTime(5),
-            accumulation_mode=beam.trigger.AccumulationMode.DISCARDING,
-        )
-
-        (
-            windowed
             | "ToJson" >> beam.Map(json.dumps)
             | "Write" >> beam.io.WriteToText(
-                file_path_prefix=OUTPUT_PATH,
+                file_path_prefix=OUTPUT_PREFIX,
                 file_name_suffix=".json",
-                num_shards=5,
-                # chave: habilita escrita para coleções não delimitadas com janelas
-                windowed_writes=True,
+                num_shards=1
             )
         )
 
