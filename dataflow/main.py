48a66ca570c4a74cf904991850acf0c9950013bf
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 15:41:57 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,48 +1,38 @@
 import json
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, SetupOptions
-from apache_beam.io.fileio import WriteToFiles, TextSink
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
-INPUT_SUB = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
-OUTPUT_BUCKET = f"cars-sales-{PROJECT_ID}-prod-events-staging"
-
+INPUT_SUB = "projects/vaulted-acolyte-462921-v2/subscriptions/cars-sales-vaulted-acolyte-462921-v2-prod-events-sub"
+OUTPUT_PATH = "gs://cars-sales-vaulted-acolyte-462921-v2-prod-events-staging/events/output"
 
 class CustomOptions(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
-        parser.add_value_provider_argument(
-            "--mode",
-            default="run",
-        )
-
+        parser.add_value_provider_argument("--mode", default="run")
 
 def run():
     options = PipelineOptions(
         project=PROJECT_ID,
         region="us-central1",
-        temp_location=f"gs://cars-sales-{PROJECT_ID}-prod-dataflow-temp/temp",
+        temp_location="gs://cars-sales-vaulted-acolyte-462921-v2-prod-dataflow-temp/temp",
         runner="DataflowRunner",
         streaming=True,
     )
-
     options.view_as(StandardOptions).streaming = True
     options.view_as(SetupOptions).save_main_session = True
     custom = options.view_as(CustomOptions)
 
     from utils.transforms import process_event
 
-    def route(record, **kwargs):
-        return "events"  # output folder name
-
     with beam.Pipeline(options=options) as p:
         events = (
             p
             | "Read" >> beam.io.ReadFromPubSub(subscription=INPUT_SUB)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            | "ParseJSON" >> beam.Map(json.loads)
+            | "Parse" >> beam.Map(json.loads)
             | "Process" >> beam.Map(process_event)
-            | "Filter" >> beam.Filter(lambda x: x is not None)
+            | "Filter" >> beam.Filter(lambda e: e is not None)
         )
 
         windowed = events | "Window" >> beam.WindowInto(
@@ -51,20 +41,24 @@ def run():
             accumulation_mode=beam.trigger.AccumulationMode.DISCARDING,
         )
 
-        if custom.mode.get() == "template":
+        def is_template():
+            try:
+                return custom.mode.get() == "template"
+            except:
+                return False  # during template build
+
+        if is_template():
             _ = windowed | "NoOp" >> beam.Map(lambda _: None)
         else:
             (
                 windowed
                 | "ToJson" >> beam.Map(json.dumps)
-                | "Write" >> WriteToFiles(
-                    path=f"gs://{OUTPUT_BUCKET}/",
-                    destination=route,
-                    file_naming=lambda dest, shard, **kw: f"output-{shard}.json",
-                    sink=lambda dest: TextSink(),
+                | "Write" >> beam.io.WriteToText(
+                    file_path_prefix=OUTPUT_PATH,
+                    file_name_suffix=".json",
+                    num_shards=5
                 )
             )
-
 
 if __name__ == "__main__":
     run()
