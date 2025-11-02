d91877806bf2fbbf3a87e9e8e4dc4a290372dc31
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 12:25:37 -0300

    fix: integration between dataflow and gcs

--- infra/modules/dataflow/iam/main.tf
+++ infra/modules/dataflow/iam/main.tf
@@ -1,12 +1,27 @@
-resource "google_storage_bucket_iam_member" "dataflow_temp" {
-  bucket = "cars-sales-${var.project_id}-${var.env}-dataflow-temp" 
+data "google_project" "project" {}
+
+# Dataflow service account (padrão do serviço)
+resource "google_storage_bucket_iam_member" "dataflow_temp_worker" {
+  bucket = "cars-sales-${var.project_id}-${var.env}-dataflow-temp"
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
+  member = "serviceAccount:service-${data.google_project.project.number}@dataflow-service-producer-prod.iam.gserviceaccount.com"
 }
 
-resource "google_storage_bucket_iam_member" "dataflow_staging" {
-  bucket = "cars-sales-${var.project_id}-${var.env}-events-staging"  
+resource "google_storage_bucket_iam_member" "dataflow_staging_worker" {
+  bucket = "cars-sales-${var.project_id}-${var.env}-events-staging"
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"
+  member = "serviceAccount:service-${data.google_project.project.number}@dataflow-service-producer-prod.iam.gserviceaccount.com"
 }
 
+# Compute default SA (workers quando Dataflow cria VMs)
+resource "google_storage_bucket_iam_member" "compute_default_temp" {
+  bucket = "cars-sales-${var.project_id}-${var.env}-dataflow-temp"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "compute_default_staging" {
+  bucket = "cars-sales-${var.project_id}-${var.env}-events-staging"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
--- infra/modules/pubsub/main.tf
+++ infra/modules/pubsub/main.tf
@@ -4,21 +4,13 @@ resource "google_pubsub_topic" "events" {
 }
 
 resource "google_pubsub_subscription" "events_sub" {
-  name  = "cars-sales-${var.project_id}-${var.env}-events-sub"
-  topic = google_pubsub_topic.events.name
+  name    = "cars-sales-${var.project_id}-${var.env}-events-sub"
+  topic   = google_pubsub_topic.events.name
   project = var.project_id
 }
 
-output "events_topic" {
-  value = google_pubsub_topic.events.name
-}
-
-output "events_subscription" {
-  value = google_pubsub_subscription.events_sub.name
-}
-
-resource "google_pubsub_topic_iam_member" "dataflow_pubsub" {
-  topic  = google_pubsub_topic.events.name  
-  role   = "roles/pubsub.subscriber"        
-  member = "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com"  # Conta do Dataflow
+resource "google_pubsub_subscription_iam_member" "dataflow_sub" {
+  subscription = google_pubsub_subscription.events_sub.name
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:dataflow-sa@${var.project_id}.iam.gserviceaccount.com"
 }
