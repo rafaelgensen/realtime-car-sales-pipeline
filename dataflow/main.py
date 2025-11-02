0c5a2fab0fa1186c1483c142c4a31c4add7220cc
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 17:56:15 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,6 +1,5 @@
-import json
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, SetupOptions
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
 SUBSCRIPTION = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
@@ -11,17 +10,24 @@ def run():
         project=PROJECT_ID,
         region="us-central1",
         runner="DataflowRunner",
-        temp_location=f"gs://cars-sales-{PROJECT_ID}-prod-dataflow-temp/temp"
+        temp_location=f"gs://cars-sales-{PROJECT_ID}-prod-dataflow-temp/temp",
+        streaming=True,
     )
-
-    options.view_as(SetupOptions).save_main_session = True
+    options.view_as(StandardOptions).streaming = True
 
     with beam.Pipeline(options=options) as p:
         (
             p
             | "ReadPubSub" >> beam.io.ReadFromPubSub(subscription=SUBSCRIPTION)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            | "WriteGCS" >> beam.io.WriteToText(
+            | "Window" >> beam.WindowInto(
+                beam.window.FixedWindows(10),
+                trigger=beam.trigger.Repeatedly(
+                    beam.trigger.AfterProcessingTime(5)
+                ),
+                accumulation_mode=beam.trigger.AccumulationMode.DISCARDING,
+            )
+            | "Write" >> beam.io.WriteToText(
                 file_path_prefix=OUTPUT_PREFIX,
                 file_name_suffix=".json",
                 num_shards=1
