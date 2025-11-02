686c67288cb11a506f4ebb51d69ebcbf2acdd49c
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 11:33:26 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,43 +1,37 @@
 import json
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.options.value_provider import RuntimeValueProvider
 from google.cloud import storage
 import google.auth
-import sys
 
-# --- CONFIG LOAD ---
-def load_config_from_gcs():
+from utils.transforms import process_event
+
+
+class CustomOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_value_provider_argument(
+            "--template_mode",
+            default=False,
+            type=bool,
+            help="True when building template"
+        )
+
+
+def load_config():
     credentials, project_id = google.auth.default()
-    bucket_name = f"cars-sales-{project_id}-prod-dataflow-temp"
-    blob_name = "config/input_output_config.json"
+    bucket = f"cars-sales-{project_id}-prod-dataflow-temp"
+    blob = "config/input_output_config.json"
     client = storage.Client(credentials=credentials, project=project_id)
-    content = client.bucket(bucket_name).blob(blob_name).download_as_text()
-    return json.loads(content), project_id
+    data = client.bucket(bucket).blob(blob).download_as_text()
+    return json.loads(data), project_id
 
-# --- PARSING ---
-def parse_ts(event):
-    from datetime import datetime
-    ts = event.get("timestamp")
-    try:
-        event["timestamp"] = datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%fZ")
-    except ValueError:
-        event["timestamp"] = datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ")
-    return event
 
-def process_event(event):
-    t = event.get("type")
-    if t == "purchase":
-        event["destination"] = "buy"
-    elif t == "cancellation":
-        event["destination"] = "cancellation"
-    else:
-        return None
-    return event
-
-# --- PIPELINE ---
 def run():
-    config, project_id = load_config_from_gcs()
-    input_subscription = f"projects/{project_id}/subscriptions/{config['input_topic']}"
+    config, project_id = load_config()
+
+    input_sub = f"projects/{project_id}/subscriptions/{config['input_topic']}"
     output_bucket = config["output_bucket"]
 
     opts = PipelineOptions(
@@ -48,40 +42,44 @@ def run():
         streaming=True,
     )
     opts.view_as(StandardOptions).streaming = True
-
-    # detect template build mode
-    is_template_build = "--template" in sys.argv
+    custom = opts.view_as(CustomOptions)
 
     with beam.Pipeline(options=opts) as p:
+
         events = (
             p
-            | "ReadPubSub" >> beam.io.ReadFromPubSub(subscription=input_subscription)
+            | "Read" >> beam.io.ReadFromPubSub(subscription=input_sub)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            | "ParseJSON" >> beam.Map(json.loads)
-            | "DropNull" >> beam.Filter(lambda e: e)
-            | "ParseTS" >> beam.Map(parse_ts)
+            | "Parse" >> beam.Map(json.loads)
             | "Process" >> beam.Map(process_event)
-            | "DropInvalid" >> beam.Filter(lambda e: e is not None)
-            | "Window" >> beam.WindowInto(
+            | "Filter valid" >> beam.Filter(lambda e: e is not None)
+            | "Window10s" >> beam.WindowInto(
                 beam.window.FixedWindows(10),
                 trigger=beam.trigger.AfterProcessingTime(5),
                 accumulation_mode=beam.trigger.AccumulationMode.DISCARDING
             )
         )
 
-        if not is_template_build:
-            (
-                events
-                | "ToStr" >> beam.Map(json.dumps)
-                | "Write" >> beam.io.WriteToText(
-                    file_path_prefix=f"gs://{output_bucket}/events/output",
-                    file_name_suffix=".json",
-                    num_shards=5
-                )
+        # Runtime: write
+        (
+            events
+            | "WriteToGCS"
+            >> beam.io.WriteToText(
+                file_path_prefix=f"gs://{output_bucket}/events/output",
+                file_name_suffix=".json",
+                num_shards=1,
             )
-        else:
-            _ = events | "Noop" >> beam.Map(lambda x: None)
+            .only_if(custom.template_mode == False)
+        )
+
+        # Template build: no-op
+        (
+            events
+            | "TemplateNoOp"
+            >> beam.FlatMap(lambda x: [])
+            .only_if(custom.template_mode == True)
+        )
+
 
 if __name__ == "__main__":
-    from utils.transforms import filter_null, process_event, write_to_gcs
     run()
