b38f39b7065ede8084a70390546297758b148c83
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 12:40:58 -0300

    fix: integration between dataflow and gcs

--- infra/modules/pubsub/main.tf
+++ infra/modules/pubsub/main.tf
@@ -1,3 +1,5 @@
+data "google_project" "project" {}
+
 resource "google_pubsub_topic" "events" {
   name    = "cars-sales-${var.project_id}-${var.env}-events"
   project = var.project_id
@@ -9,27 +11,23 @@ resource "google_pubsub_subscription" "events_sub" {
   project = var.project_id
 }
 
-# Permite ao Dataflow ler da subscription
+# Dataflow lê da subscription
 resource "google_pubsub_subscription_iam_member" "dataflow_sub" {
   subscription = google_pubsub_subscription.events_sub.name
   role         = "roles/pubsub.subscriber"
-
-  # Conta padrão de execução do Dataflow (managed service identity)
-  member = "serviceAccount:service-${var.project_id}@dataflow-service-producer-prod.iam.gserviceaccount.com"
+  member       = "serviceAccount:service-${data.google_project.project.number}@dataflow-service-producer-prod.iam.gserviceaccount.com"
 }
 
-# Permite ao Dataflow publicar (se no futuro você mandar output para Pub/Sub)
+# Compute default SA (workers)
+resource "google_pubsub_subscription_iam_member" "compute_sub" {
+  subscription = google_pubsub_subscription.events_sub.name
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Dataflow publica (caso use Pub/Sub output)
 resource "google_pubsub_topic_iam_member" "dataflow_pub" {
-  topic = google_pubsub_topic.events.name
-  role  = "roles/pubsub.publisher"
-
-  member = "serviceAccount:service-${var.project_id}@dataflow-service-producer-prod.iam.gserviceaccount.com"
-}
-
-output "events_topic" {
-  value = google_pubsub_topic.events.name
-}
-
-output "events_subscription" {
-  value = google_pubsub_subscription.events_sub.name
+  topic  = google_pubsub_topic.events.name
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:service-${data.google_project.project.number}@dataflow-service-producer-prod.iam.gserviceaccount.com"
 }
