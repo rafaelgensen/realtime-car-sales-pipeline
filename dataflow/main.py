f5d43ecc0b895611a49f6c5843f29bfd97af0003
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 18:10:01 -0300

    fix: dataflow integrations

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,31 +1,33 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 from apache_beam import window
+import json
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
-SUBSCRIPTION = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
-OUTPUT_PREFIX = f"gs://cars-sales-{PROJECT_ID}-prod-events-staging/events/output"
+SUB = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
+OUT = f"gs://cars-sales-{PROJECT_ID}-prod-events-staging/events/output"
 
 def run():
-    pipeline_options = PipelineOptions(
+    opts = PipelineOptions(
         project=PROJECT_ID,
         region="us-central1",
         runner="DataflowRunner",
         temp_location=f"gs://cars-sales-{PROJECT_ID}-prod-dataflow-temp/temp",
         streaming=True
     )
-    pipeline_options.view_as(StandardOptions).streaming = True
+    opts.view_as(StandardOptions).streaming = True
 
-    with beam.Pipeline(options=pipeline_options) as p:
+    with beam.Pipeline(options=opts) as p:
         (
             p
-            | "ReadPubSub" >> beam.io.ReadFromPubSub(subscription=SUBSCRIPTION)
-            | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            # obrigar janela cedo para evitar GBK global
+            | "Read" >> beam.io.ReadFromPubSub(subscription=SUB)
+            | "Decode" >> beam.Map(lambda b: b.decode("utf-8"))
+            | "ParseJSON" >> beam.Map(lambda x: json.loads(x))
             | "Window5s" >> beam.WindowInto(window.FixedWindows(5))
-            | "WriteGCS" >> beam.io.WriteToText(
-                file_path_prefix=OUTPUT_PREFIX,
-                file_name_suffix=".txt",
+            | "ToString" >> beam.Map(lambda x: json.dumps(x))
+            | "Write" >> beam.io.WriteToText(
+                file_path_prefix=OUT,
+                file_name_suffix=".json",
                 num_shards=1
             )
         )
