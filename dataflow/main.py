eb74ed454b4e37d55e2fd70bf460730b4eec3265
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 18:01:31 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,35 +1,31 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam import window
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
 SUBSCRIPTION = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
 OUTPUT_PREFIX = f"gs://cars-sales-{PROJECT_ID}-prod-events-staging/events/output"
 
 def run():
-    options = PipelineOptions(
+    pipeline_options = PipelineOptions(
         project=PROJECT_ID,
         region="us-central1",
         runner="DataflowRunner",
         temp_location=f"gs://cars-sales-{PROJECT_ID}-prod-dataflow-temp/temp",
-        streaming=True,
+        streaming=True
     )
-    options.view_as(StandardOptions).streaming = True
+    pipeline_options.view_as(StandardOptions).streaming = True
 
-    with beam.Pipeline(options=options) as p:
+    with beam.Pipeline(options=pipeline_options) as p:
         (
             p
             | "ReadPubSub" >> beam.io.ReadFromPubSub(subscription=SUBSCRIPTION)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            | "Window" >> beam.WindowInto(
-                beam.window.FixedWindows(10),
-                trigger=beam.trigger.Repeatedly(
-                    beam.trigger.AfterProcessingTime(5)
-                ),
-                accumulation_mode=beam.trigger.AccumulationMode.DISCARDING,
-            )
-            | "Write" >> beam.io.WriteToText(
+            # obrigar janela cedo para evitar GBK global
+            | "Window5s" >> beam.WindowInto(window.FixedWindows(5))
+            | "WriteGCS" >> beam.io.WriteToText(
                 file_path_prefix=OUTPUT_PREFIX,
-                file_name_suffix=".json",
+                file_name_suffix=".txt",
                 num_shards=1
             )
         )
