a3bd9d19fc3d462c1a5747be2ac39ccd1bcca72d
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 13:25:28 -0300

    fix: integration between dataflow and gcs

--- infra/modules/dataflow/job/main.tf
+++ infra/modules/dataflow/job/main.tf
@@ -1,12 +1,16 @@
 resource "google_dataflow_job" "streaming" {
   name              = var.job_name
-  template_gcs_path = "gs://cars-sales-${var.project_id}-${var.env}-dataflow-temp/template/main-template"
+  template_gcs_path = null
   temp_gcs_location = "gs://cars-sales-${var.project_id}-${var.env}-dataflow-temp/temp"
   region            = var.region
   on_delete         = "cancel"
+
+  parameters = {
+    input_subscription = "projects/${var.project_id}/subscriptions/cars-sales-${var.project_id}-${var.env}-events-sub"
+    output_bucket      = "cars-sales-${var.project_id}-${var.env}-events-staging"
+  }
 }
 
 output "dataflow_job_id" {
   value = google_dataflow_job.streaming.id
 }
-
