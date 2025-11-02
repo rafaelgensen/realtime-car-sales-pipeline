5bfad891bda5b3f4a5b8056eec8b345643a441da
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-01 21:42:58 -0300

    build: infra + src py

--- infra/modules/gcs/main.tf
+++ infra/modules/gcs/main.tf
@@ -13,15 +13,11 @@ resource "google_storage_bucket" "events_staging" {
 # Upload do arquivo JSON com a configuração para o bucket
 resource "google_storage_bucket_object" "config_file" {
   name   = "config/input_output_config.json"
-  bucket = google_storage_bucket.dataflow_temp.name
+  bucket = "cars-sales-${var.project_id}-${var.env}-dataflow-temp"
   content = jsonencode({
     input_topic   = "${var.project_id}-${var.env}-events-sub"
     output_bucket = "cars-sales-${var.project_id}-${var.env}-events-staging"
   })
-}
-
-output "temp_location" {
-  value = google_storage_bucket.dataflow_temp.name
 }
 
 output "events_staging_location" {
