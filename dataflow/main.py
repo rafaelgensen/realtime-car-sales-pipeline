ec599ed9d37c335d5b9cd4e203fd043ae84ecc6d
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-02 15:37:38 -0300

    fix: integration between dataflow and gcs

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,6 +1,7 @@
 import json
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, SetupOptions
+from apache_beam.io.fileio import WriteToFiles, TextSink
 
 PROJECT_ID = "vaulted-acolyte-462921-v2"
 INPUT_SUB = f"projects/{PROJECT_ID}/subscriptions/cars-sales-{PROJECT_ID}-prod-events-sub"
@@ -12,9 +13,7 @@ class CustomOptions(PipelineOptions):
     def _add_argparse_args(cls, parser):
         parser.add_value_provider_argument(
             "--mode",
-            type=str,
             default="run",
-            help="template or run"
         )
 
 
@@ -26,40 +25,44 @@ def run():
         runner="DataflowRunner",
         streaming=True,
     )
+
     options.view_as(StandardOptions).streaming = True
     options.view_as(SetupOptions).save_main_session = True
     custom = options.view_as(CustomOptions)
 
     from utils.transforms import process_event
 
+    def route(record, **kwargs):
+        return "events"  # output folder name
+
     with beam.Pipeline(options=options) as p:
         events = (
             p
             | "Read" >> beam.io.ReadFromPubSub(subscription=INPUT_SUB)
             | "Decode" >> beam.Map(lambda x: x.decode("utf-8"))
-            | "Parse" >> beam.Map(json.loads)
+            | "ParseJSON" >> beam.Map(json.loads)
             | "Process" >> beam.Map(process_event)
-            | "FilterNone" >> beam.Filter(lambda e: e is not None)
+            | "Filter" >> beam.Filter(lambda x: x is not None)
         )
 
         windowed = events | "Window" >> beam.WindowInto(
             beam.window.FixedWindows(10),
             trigger=beam.trigger.AfterProcessingTime(5),
-            accumulation_mode=beam.trigger.AccumulationMode.DISCARDING
+            accumulation_mode=beam.trigger.AccumulationMode.DISCARDING,
         )
 
-        if custom.mode.is_accessible() and custom.mode.get() == "template":
+        if custom.mode.get() == "template":
             _ = windowed | "NoOp" >> beam.Map(lambda _: None)
         else:
             (
                 windowed
                 | "ToJson" >> beam.Map(json.dumps)
-                | "WriteText" >> beam.io.WriteToText(
-                    file_path_prefix=f"gs://{OUTPUT_BUCKET}/events/output",
-                    file_name_suffix=".json",
-                    num_shards=5,
-                    shard_name_template="-SSSSS-of-NNNNN",
-                ).with_windowed_writes()
+                | "Write" >> WriteToFiles(
+                    path=f"gs://{OUTPUT_BUCKET}/",
+                    destination=route,
+                    file_naming=lambda dest, shard, **kw: f"output-{shard}.json",
+                    sink=lambda dest: TextSink(),
+                )
             )
 
 
